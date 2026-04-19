8a59a83af98bb3075bbca59108eaf15d98463cce
dcth0002-art <dcth0002@gmail.com>
2026-04-19 12:35:19 +0900

    Add files via upload

--- config.py
+++ config.py
@@ -16,11 +16,11 @@ ACTION_SPACE = 3
 LEARNING_RATE = 0.0003 # Tốc độ học chậm lại một chút để chắc chắn hơn
 DISCOUNT_FACTOR = 0.99 
 EPSILON_START = 1.0 
-EPSILON_END = 0.05 # Không nên để 0.01, giữ 5% ngẫu nhiên để bot luôn nhạy bén
-EPSILON_DECAY_STEPS = 300000 # Cho bot trải nghiệm nhiều hơn trước khi chốt bài học
+EPSILON_END = 0.05 # Giữ 5% ngẫu nhiên để bot luôn nhạy bén
+EPSILON_DECAY_STEPS = 300000 
 
-# Cấu hình cho Replay Buffer
-BUFFER_SIZE = 50000 
+# Cấu hình cho Replay Buffer - Tăng ký ức lên 200,000
+BUFFER_SIZE = 200000 
 BATCH_SIZE = 128 
 
 MODEL_SAVE_PATH = 'saved_models/trading_bot_model.weights.h5'
